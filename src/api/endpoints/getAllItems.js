03f37dbadadf0d1acdd02c4b9a77452a47af6d13
Brun0Silveir4 <bruno.dionisio@fatec.sp.gov.br>
2024-11-12 11:54:04 -0300

    updating getAllItems to recieve last item from all days

--- src/api/endpoints/getAllItems.js
+++ src/api/endpoints/getAllItems.js
@@ -1,23 +1,50 @@
-const db = require('../config')
+const db = require('../config');
 
 const getAllItems = async (req, res) => {
     try {
-      const response = await db.collection("SensorData").get();
+        const response = await db.collection("SensorData").get();
+        const entriesByDay = {};
 
-      let responseArr = [];
+        response.forEach((data) => {
+            const docData = data.data();
+            const docDate = docData.date; 
+            
+            if (!entriesByDay[docDate] || compareTimes(docData.time, entriesByDay[docDate].time)) {
+                entriesByDay[docDate] = docData; 
+                console.log(entriesByDay[docDate])
+            }
+        });
 
-      response.forEach((doc) => {
-        responseArr.push(doc.data());
-      });
+        const sortedEntries = Object.values(entriesByDay).sort((a, b) => {
+            const [dayA, monthA, yearA] = a.date.split("/").map(Number);
+            const [dayB, monthB, yearB] = b.date.split("/").map(Number);
 
-      if (responseArr.length >= 1) {
-        res.status(200).json(responseArr);
-      } else {
-        res.status(400).json({ error: "No data found" });
-      }
+            const dateA = new Date(yearA, monthA - 1, dayA);
+            const dateB = new Date(yearB, monthB - 1, dayB);
+
+            return dateB - dateA; 
+        });
+
+        console.log(sortedEntries)
+
+        if (sortedEntries.length > 0) {
+            res.status(200).json(sortedEntries);
+        } else {
+            res.status(404).json({ error: 'No data found' });
+        }
     } catch (e) {
-      res.status(400).json({ error: e });
+        res.status(500).json({ error: "Internal Server Error" });
     }
-  }
+};
 
-module.exports = getAllItems
+function compareTimes(time1, time2) {
+    const [hour1, minute1] = time1.split(":").map(Number);
+    const [hour2, minute2] = time2.split(":").map(Number);
+
+    if (hour1 !== hour2) {
+        return hour1 > hour2; 
+    }
+    return minute1 > minute2; 
+}
+
+module.exports = getAllItems;
